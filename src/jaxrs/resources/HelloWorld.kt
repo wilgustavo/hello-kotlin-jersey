fb732cea6d7a1e0ce0dd131bb23f66f52fcb7316
wilgenho <wilgenho@lamercantil.com.ar>
2018-08-17 20:05:05 -0300

    Add Json Handling

--- src/jaxrs/resources/HelloWorld.kt
+++ src/jaxrs/resources/HelloWorld.kt
@@ -2,10 +2,20 @@ package jaxrs.resources
 
 import javax.ws.rs.GET
 import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
 
 @Path("hola")
 class HelloWorldResource {
 	
 	@GET
 	fun helloWorld() = "Hola Mundo!"
+	
+	data class HelloJson(val id: Int, val name: String)
+	
+	@GET
+	@Path("json")
+	@Produces(MediaType.APPLICATION_JSON)
+	fun helloJson() = HelloJson(1, "test")
+	
 }

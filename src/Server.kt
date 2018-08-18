984c75ec1ac4ab52face7a67afeb53a6912cdf74
Gustavo Wilgenhoff <wilgustavo@gmail.com>
2018-08-18 18:40:28 -0300

    Quitar comentarios

--- src/Server.kt
+++ src/Server.kt
@@ -3,12 +3,8 @@ import javax.ws.rs.core.UriBuilder
 import org.glassfish.jersey.netty.httpserver.NettyHttpContainerProvider
 
 fun main(args: Array<String>) {
-	
 	val url = UriBuilder.fromUri("http://0.0.0.0/").port(8080).build()
-	
-	//val httpServer = GrizzlyHttpServerFactory.createHttpServer(url, Application(), true)
 	val httpServer = NettyHttpContainerProvider.createHttp2Server(url, Application(), null)
-	
 	
     if (System.getenv()["SHUTDOWN_TYPE"].equals("INPUT")) {
         println("Press any key to shutdown")

3e11302f98dcf9d610094d9304a5613d76a3c498
wilgenho <wilgenho@lamercantil.com.ar>
2018-08-18 18:31:49 -0300

    change to Netty

--- src/Server.kt
+++ src/Server.kt
@@ -1,22 +1,24 @@
 import jaxrs.Application
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory
 import javax.ws.rs.core.UriBuilder
+import org.glassfish.jersey.netty.httpserver.NettyHttpContainerProvider
 
 fun main(args: Array<String>) {
 	
 	val url = UriBuilder.fromUri("http://0.0.0.0/").port(8080).build()
 	
-	val httpServer = GrizzlyHttpServerFactory.createHttpServer(url, Application(), true)
+	//val httpServer = GrizzlyHttpServerFactory.createHttpServer(url, Application(), true)
+	val httpServer = NettyHttpContainerProvider.createHttp2Server(url, Application(), null)
+	
 	
     if (System.getenv()["SHUTDOWN_TYPE"].equals("INPUT")) {
         println("Press any key to shutdown")
         readLine()
         println("Shutting down from input")
-        httpServer.shutdownNow()
+        httpServer.close()
     } else {
         Runtime.getRuntime().addShutdownHook(Thread {
             println("Shutting down from shutdown hook")
-            httpServer.shutdownNow()
+            httpServer.close()
         })
 
         println("Press Ctrl+C to shutdown")
